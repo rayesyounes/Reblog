b0989897e6a0f6279d11814fb7367e6081d18f9f
rayesyounes <rayesyounes88@gmail.com>
2024-04-13 13:17:16 +0000

    create post / categories migration

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Post;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -19,5 +21,9 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        Post::factory(50)->create();
+        Category::factory(10)->create();
+
     }
 }
--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -1,3 +1,5 @@
 @props(['disabled' => false])
 
-<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm']) !!}>
+<label>
+    <input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'bg-blue-50 focus:outline-none focus:border-none focus:ring-0 outline-none border-none text-xs text-gray-800 placeholder:text-gray-400']) !!}>
+</label>
