53ccf83d56ec4c82bd129c65ab11ba1067bb41c8
rayesyounes <rayesyounes88@gmail.com>
2024-04-14 01:39:38 +0000

    update

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,11 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Carbon\Carbon;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Str;
 
 class Post extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
     protected function casts(): array
     {
